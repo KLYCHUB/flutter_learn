56e114e903f5ce509442cff43410aba439003f4d
Eren KALAYCI <klychub@gmail.com>
2023-10-27 16:24:09 +0300

    Services

---
+++ lib/202/service/service_learn_view.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'coments_learn_view.dart';
+import 'post_model.dart';
+import 'post_service.dart';
+
+class ServiceLearn extends StatefulWidget {
+  const ServiceLearn({Key? key}) : super(key: key);
+
+  @override
+  State<ServiceLearn> createState() => _ServiceLearnState();
+}
+
+class _ServiceLearnState extends State<ServiceLearn> {
+  List<PostModel>? _items;
+  String? name;
+  bool _isLoading = false;
+  // ignore: unused_field
+  late final Dio _dio;
+  final _baseUrl = 'https://jsonplaceholder.typicode.com/';
+
+  // TEST EDIELBIR KOD BASLADI
+  late final IPostService _postService;
+
+  @override
+  void initState() {
+    super.initState();
+    _dio = Dio(BaseOptions(baseUrl: _baseUrl));
+    _postService = PostService();
+    name = 'veli';
+    fetchPostItemsAdvance();
+  }
+
+  void _changeLoading() {
+    setState(() {
+      _isLoading = !_isLoading;
+    });
+  }
+
+  Future<void> fetchPostItems() async {
+    _changeLoading();
+    final response =
+        await Dio().get('https://jsonplaceholder.typicode.com/posts');
+
+    if (response.statusCode == HttpStatus.ok) {
+      // ignore: no_leading_underscores_for_local_identifiers
+      final _datas = response.data;
+
+      if (_datas is List) {
+        setState(() {
+          _items = _datas.map((e) => PostModel.fromJson(e)).toList();
+        });
+      }
+    }
+    _changeLoading();
+  }
+
+  Future<void> fetchPostItemsAdvance() async {
+    _changeLoading();
+    _items = await _postService.fetchPostItemsAdvance();
+    _changeLoading();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name ?? ''),
+        actions: [
+          _isLoading
+              ? const CircularProgressIndicator.adaptive()
+              : const SizedBox.shrink()
+        ],
+      ),
+      body: _items == null
+          ? const Placeholder()
+          : ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              itemCount: _items?.length ?? 0,
+              itemBuilder: (context, index) {
+                return _PostCard(model: _items?[index]);
+              },
+            ),
+    );
+  }
+}
+
+class _PostCard extends StatelessWidget {
+  const _PostCard({
+    Key? key,
+    required PostModel? model,
+  })  : _model = model,
+        super(key: key);
+
+  final PostModel? _model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => CommentsLearnView(postId: _model?.id),
+          ));
+        },
+        title: Text(_model?.title ?? ''),
+        subtitle: Text(_model?.body ?? ''),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_learn/202/service/services_post_learn.dart';
 
 import '101/color_learn.dart';
-import '202/model_learn_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
           systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
       ),
-      home: const ModelLearnView(),
+      home: const ServicePostLearn(),
     );
   }
 }
